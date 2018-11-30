3e5c0a99864abeb00b2517b203f8824d1c9d92cd
KarimM7mad <KarimMohamed95@hotmail.com>
2018-11-30 11:17:23 +0200

    all Done

--- app/src/main/java/com/example/karimm7mad/sensorbasedauth/MainActivity.java
+++ app/src/main/java/com/example/karimm7mad/sensorbasedauth/MainActivity.java
@@ -1,17 +1,20 @@
 package com.example.karimm7mad.sensorbasedauth;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
@@ -19,7 +22,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     public TextView msgTxt, debugTxt, sensorReadingTxt;
     public Button startTimerBtn, stopTimerBtn;
-    public CountDownTimer downTimer;
+    public CountDownTimer mainDownTimer, subDownTimer;
     private SensorManager sensMan;
     private Sensor linSensor;
     public int numOfShakesInInterval;
@@ -27,6 +30,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private static final float SHAKE_THRESHOLD = 3.25f; // m/S**2
     private static final int MIN_TIME_BETWEEN_SHAKES_MILLISECS = 550;
     private long mLastShakeTime;
+    public boolean startedTimer = false;
+    public ProgressBar loading;
+    public static final String patternToFind = "1-2-3";
 
 
     @Override
@@ -38,19 +44,24 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         this.msgTxt = findViewById(R.id.msgTxt);
         this.debugTxt = findViewById(R.id.debugTxt);
         this.sensorReadingTxt = findViewById(R.id.sensorReadings);
+        this.loading = findViewById(R.id.determinateBar);
+        this.loading.setProgress(100);
         this.numOfShakesInInterval = 0;
         //sensor itself
         this.sensMan = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         this.linSensor = sensMan.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         //create Timer Object
-        this.createTimeer();
+        msgTxt.setText("");
+        debugTxt.setText("Click on \"Start Timer\" to Proceed");
+        this.createTimers();
         //startTimerbtn
         this.startTimerBtn = findViewById(R.id.startTimerbtn);
         this.startTimerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                msgTxt.setText("");
-                downTimer.start();
+                mainDownTimer.start();
+                subDownTimer.start();
+                debugTxt.setText("");
                 Log.d("asdasd", "debugyAna: Timer Started");
             }
         });
@@ -59,38 +70,62 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         this.stopTimerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                mainDownTimer.cancel();
+                subDownTimer.cancel();
+                startedTimer = false;
+                loading.setProgress(100);
+                msgTxt.setText("");
+                debugTxt.setText("Click on \"Start Timer\" to Proceed");
                 Log.d("asdasd", "debugyAna: Timer Stopped");
-                downTimer.cancel();
             }
         });
 
     }
 
 
-    public void createTimeer() {
+    public void createTimers() {
         msgTxt.setText("");
-        this.downTimer = new CountDownTimer(10000, 3000) {
+        this.mainDownTimer = new CountDownTimer(10000, 3000) {
             @Override
             public void onTick(long millisUntilFinished) {
+                subDownTimer.cancel();
+                loading.setProgress(0);
                 Log.d("asdasd", "debugyAna: Seconds Remaining->" + (millisUntilFinished / 1000));
-//                Log.d("asdasd", "debugyAna: NumOfShakes->" + numOfShakesInInterval);
-
-                msgTxt.append(numOfShakesInInterval + "-");
-
+                if (startedTimer)
+                    msgTxt.append(numOfShakesInInterval + "-");
+                else
+                    startedTimer = true;
                 numOfShakesInInterval = 0;
+                subDownTimer.start();
             }
 
             @Override
             public void onFinish() {
                 Log.d("asdasd", "debugyAna: Timer is Finished");
-
-
-                msgTxt.append(numOfShakesInInterval + "-");
-
+                msgTxt.append(numOfShakesInInterval + "");
                 numOfShakesInInterval = 0;
+                startedTimer = false;
+                if (msgTxt.getText().toString().equalsIgnoreCase(MainActivity.patternToFind))
+                    debugTxt.setText("Password Correct");
+                else {
+                    debugTxt.setText("Wrong Password, Try Again");
+                    msgTxt.setBackgroundColor(getResources().getColor(R.color.red, getTheme()));
+                }
+            }
+        };
+        this.subDownTimer = new CountDownTimer(3000, 150) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                loading.setProgress((int) ((3000 - millisUntilFinished) / 30));
+            }
+
+            @Override
+            public void onFinish() {
+                loading.setProgress(100);
             }
         };
     }
+
 
     @Override
     protected void onResume() {
@@ -109,18 +144,14 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
             long curTime = System.currentTimeMillis();
             if ((curTime - mLastShakeTime) > MIN_TIME_BETWEEN_SHAKES_MILLISECS) {
-
                 float x = event.values[0];
                 float y = event.values[1];
                 float z = event.values[2];
-
                 double acceleration = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2)) - SensorManager.GRAVITY_EARTH;
-//                Log.d("asdasd", "Acceleration is " + acceleration + "m/s^2");
-
                 if (acceleration > SHAKE_THRESHOLD) {
                     mLastShakeTime = curTime;
                     numOfShakesInInterval++;
-                    Log.d("asdasd", "Shake, Rattle, and Roll");
+                    Log.d("asdasd", "Device Shaked");
                 }
             }
         }
@@ -132,4 +163,5 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
+
 }
